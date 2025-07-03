51f375893d957f714aadef47123030e030c25a31
luis-navarro <inluisantonio2.2@gmail.com>
2025-07-03 09:13:51 -0500

    v5.1 Corrigiendo errores y mejorando CRUD API RESTful de posts

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -1,20 +1,51 @@
 from rest_framework import serializers
 from posts.models import Post
+from categories.models import Category
 from users.api.serializers import UserInfoSerializer
 from categories.api.serializers import CategoryInfoSerializer
 
 class PostSerializer(serializers.ModelSerializer):
-    user = UserInfoSerializer()
-    category = CategoryInfoSerializer()
+    # Solo lectura (para mostrar info del usuario)
+    user = UserInfoSerializer(read_only=True)
+    # Solo lectura (para mostrar info de la categoria)
+    category = CategoryInfoSerializer(read_only=True)
+
+    # Solo escritura (acepta ID)
+    category_id = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(),  
+        write_only=True, 
+        source='category',
+        required=False #Necesario para permitir usar slug como alternativa
+    )
+    #Solo escritura (acepta ID)
+    category_slug = serializers.SlugRelatedField(
+        queryset=Category.objects.all(),
+        slug_field='slug', 
+        write_only=True, 
+        source='category',
+        required=False #Necesario para permitir usar id como alternativa
+    )
 
     class Meta:
         model = Post
-        fields = ['id', 'title', 'content', 'slug', 'image', 'published', 'created_at', 'user', 'category']
+        fields = [
+            'id', 'title', 'content', 'slug', 'image', 'published', 'created_at', 'user', 'category', 
+            'category_id', 'category_slug'
+        ]
         read_only_fields = ('slug', 'created_at', 'user')
 
         extra_kwargs = {
             'category': {'required': True, 'allow_null': False},
-        }  
+        }
+
+    def validate(self, data):
+        # Validar que al menos se haya enviado uno de los dos campos "category_id o category_slug"
+        #if not data.get('category'):
+        if 'category' not in data and not self.instance:
+            raise serializers.ValidationError({
+                'category': 'Debe proporcionar category_id o category_slug'
+            })
+        return data
 
 
 
